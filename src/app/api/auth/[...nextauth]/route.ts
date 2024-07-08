d62f5a49f0d92bde6cb61a60f078283efb74ee9d
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-08 10:25:48 +0600

    Updated: added credentials signin demo

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,46 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/firebase";
 
 export const authOptions = {
+  pages: {
+    signIn: "/signin",
+  },
   // Configure one or more authentication providers
   providers: [
+    // email provider
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials): Promise<any> {
+        return await signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || "",
+          (credentials as any).password || ""
+        )
+          .then((userCredential) => {
+            if (userCredential.user) {
+              return userCredential.user;
+            }
+            return null;
+          })
+          .catch((error) => console.log(error))
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error);
+          });
+      },
+    }),
+    // github provider
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
-    // ...add more providers here
   ],
-}
+};
 
 export const handler = NextAuth(authOptions);
 
