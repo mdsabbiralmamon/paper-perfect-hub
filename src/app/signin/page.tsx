6c2adeb5acd775469e8b74e51ae0b8a9ba682372
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-09 09:48:25 +0600

    Updated: modified for light theme

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -8,16 +9,16 @@ export default function Signin() {
   const [password, setPassword] = useState('');
   const router = useRouter();
 
-    const handleGithubSignIn = async () => {
-        await signIn('github', { callbackUrl: '/' });
-    };
+  const handleGithubSignIn = async () => {
+    await signIn('github', { callbackUrl: '/' });
+  };
 
   return (
     <>
-    <div className="flex bg-black min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          <Image src="/logo.png" alt="logo" width={100} height={100} />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
@@ -25,7 +26,7 @@ export default function Signin() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-700">
                 Email address
               </label>
               <div className="mt-2">
@@ -36,18 +37,18 @@ export default function Signin() {
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block p-4 w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-700">
                   Password
                 </label>
                 <div className="text-sm">
-                  <div onClick={() => router.push('/forgot-password')} className="cursor-pointer font-semibold text-indigo-400 hover:text-indigo-300">
+                  <div onClick={() => router.push('/forgot-password')} className="cursor-pointer font-semibold text-indigo-500 hover:text-indigo-400">
                     Forgot password?
                   </div>
                 </div>
@@ -60,31 +61,37 @@ export default function Signin() {
                   autoComplete="current-password"
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block p-4 w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <button
-                onClick={() => signIn('credentials', {email, password, redirect: true, callbackUrl: '/'})}
+                onClick={() => signIn('credentials', { email, password, redirect: true, callbackUrl: '/' })}
                 disabled={!email || !password}
-                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign in
               </button>
             </div>
           </div>
 
-          <p className="mt-10 text-center text-sm text-gray-400">
+          <p className="mt-10 text-center text-sm text-gray-600">
             Not a member?{' '}
-            <button onClick={() => router.push('signup')} className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300">
+            <button onClick={() => router.push('signup')} className="font-semibold leading-6 text-indigo-500 hover:text-indigo-400">
               Sign Up
             </button>
           </p>
-        </div>
-        <div>
-            <button onClick={handleGithubSignIn} className='btn btn-ghost text-white border-gray-400'>sign in with github</button>
+          {/* social login buttons */}
+          <div className='mt-4 flex items-center justify-center'>
+            <div className='text-gray-700 w-full'>
+              <div className="divider divider-primary w-full">Or continue with</div>
+              <div className='mt-10'>
+              <button onClick={handleGithubSignIn} className='btn btn-ghost text-gray-700 border-gray-300'>Sign in with GitHub</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
